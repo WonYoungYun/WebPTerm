5f8c5cdd6a525c6914ebc44e4ad6a675f094d25e
wonyoung <asmar20288@naver.com>
2018-06-07 14:15:46 +0900

    qnaitem

--- term-resume/src/main/java/kr/ac/springboot/term/qna/QnAController.java
+++ term-resume/src/main/java/kr/ac/springboot/term/qna/QnAController.java
@@ -4,17 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/qna/")
 public class QnAController {
 	
 	@Autowired
 	private QnARepository repo;
 	
-	@GetMapping("/qna")
+	@GetMapping("/list")
 	public void qnAIndex(Model model) {
 		model.addAttribute("result", repo.findAllByOrderByRegdateDesc());
 	}
+	
+    @GetMapping("/{qno}")
+    public String view(@PathVariable("qno") long qno, Model model) {
+        if (repo.findById(qno).isPresent()) {
+            model.addAttribute("result", repo.findById(qno).get());
+        } else {
+            return "errors/404";
+        }
+        return "qna/item";
+    }
+    
 	
 }
